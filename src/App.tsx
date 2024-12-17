77105d15d61b3642ea94b64ea228b1f2e138322e
Andrii Yanchuk <horeca288@gmail.com>
2024-12-17 12:32:30 +0200

    first task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import React, { useEffect, useState } from 'react';
+import { Todo } from './types/Todo';
+import { Filter } from './types/Filters';
+import { Errors } from './types/Errors';
+import { TodoList } from './components/TodoList';
+import { TodoFooter } from './components/TodoFooter';
+import { TodoHeader } from './components/TodoHeader';
+import { ErrorNotification } from './components/ErrorNotification';
+import { getTodos } from './api/todos';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [status, setStatus] = useState(Filter.All);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const filteredTodos = todos?.filter(todo => {
+    if (status === Filter.Completed) {
+      return todo.completed;
+    } else if (status === Filter.Active) {
+      return todo.completed === false;
+    } else {
+      return true;
+    }
+  });
+
+  const unfulfilledTodos = todos?.filter(todo => !todo.completed);
+
+  useEffect(() => {
+    getTodos()
+      .then(data => setTodos(data))
+      .catch(() => {
+        setErrorMessage(Errors.UnableToLoad);
+        const timer = setTimeout(() => setErrorMessage(''), 3000);
+
+        return () => clearTimeout(timer);
+      });
+  }, []);
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <TodoHeader />
+
+        <TodoList todos={filteredTodos} />
+        {todos.length !== 0 && (
+          <TodoFooter
+            status={status}
+            setStatus={setStatus}
+            unfulfilledTodos={unfulfilledTodos}
+          />
+        )}
+      </div>
+
+      {/* DON'T use conditional rendering to hide the notification */}
+      {/* Add the 'hidden' class to hide the message smoothly */}
+
+      <ErrorNotification errorMessage={errorMessage} />
+    </div>
   );
 };
